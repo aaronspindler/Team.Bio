141c9ec26be3256fcb716df7619a46ec0d5c1949
Aaron Spindler <aaron@xnovax.net>
2022-08-10 23:42:52 -0400

    Removed some boilerplate

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about"),
 ]
