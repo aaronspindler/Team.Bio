1fee13b8babcaa969285e8ae53d844a9398ad372
Aaron Spindler <aaron@xnovax.net>
2022-08-13 15:23:47 -0400

    Adjusting company home

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', ]
+    list_display = ['email', 'username', 'company', 'last_login']
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
