069c3b7e996161e072f3c9b35daeec246189d176
Aaron Spindler <aaron@xnovax.net>
2022-08-13 14:44:32 -0400

    Added company to user

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 
+from django.db import models
+
 
 class CustomUser(AbstractUser):
-    pass
+    company = models.ForeignKey("companies.Company", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.email
