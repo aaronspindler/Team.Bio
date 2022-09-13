84525dbf5fd9e7cc20ceb5cbac6a00a72451505a
Aaron Spindler <aaron@xnovax.net>
2022-09-13 17:09:07 -0400

    Working on adding a decorator to check if the user is an owner

---
+++ companies/decorators.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from django.http import Http404
+
+from companies.models import CompanyOwner
+
+
+def is_company_owner(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        if CompanyOwner.objects.filter(owner=request.user, company=request.user.company).exists():
+            return function(request, *args, **kwargs)
+        else:
+            raise Http404
+
+    return wrap
