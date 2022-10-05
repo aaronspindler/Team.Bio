3b798caf76af3d6e2f1eedc4932753990f5ac6c8
Aaron Spindler <aaron@xnovax.net>
2022-10-05 19:15:05 -0400

    Added sentry

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,25 @@ from pathlib import Path
 
 import environ
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://7293ea960f6a43fba4b4f73fe60bb6bc@o555567.ingest.sentry.io/4503933521231872",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False),
