04b24b85b69f672da90865ddd85cc9bd6eb8b657
Aaron Spindler <aaron@xnovax.net>
2023-01-28 17:19:49 -0500

    Fixed billing URL redirects

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ INTERNAL_IPS = ["127.0.0.1"]
 AUTH_USER_MODEL = "accounts.User"
 
 SITE_ID = 1
-BASE_URL = "https://team.bio"
+BASE_URL = "https://team.bio/"
 LOGIN_REDIRECT_URL = "company_home"
 ACCOUNT_LOGOUT_REDIRECT_URL = "home"
 AUTHENTICATION_BACKENDS = (
