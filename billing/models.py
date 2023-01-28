eb7ff9c6ace99dbc83452b11794ac3e9a0b76885
Aaron Spindler <aaron@xnovax.net>
2023-01-28 15:11:50 -0500

    Renamed some variables

--- billing/models.py
+++ billing/models.py
@@ -3,14 +3,18 @@ from django.conf import settings
 from django.db import models
 
 
+class PaymentAttempt(models.Model):
+    pass
+
+
 class StripeCustomer(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     user = models.OneToOneField("accounts.User", on_delete=models.CASCADE)
-    stripeCustomerId = models.CharField(max_length=255)
-    setupIntentId = models.CharField(max_length=255)
-    paymentMethod = models.CharField(max_length=255)
+    stripe_customer_id = models.CharField(max_length=255)
+    setup_intent_id = models.CharField(max_length=255)
+    payment_method = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user.username
@@ -24,8 +28,8 @@ class StripeCustomer(models.Model):
         stripe.PaymentIntent.create(
             amount=amount,
             currency="usd",
-            customer=self.stripeCustomerId,
-            payment_method=self.paymentMethod,
+            customer=self.stripe_customer_id,
+            payment_method=self.payment_method,
             off_session=True,
             confirm=True,
         )
@@ -37,17 +41,17 @@ class StripeCustomer(models.Model):
         """
         stripe.api_key = settings.STRIPE_SECRET_KEY
         invoice = stripe.Invoice.create(
-            customer=self.stripeCustomerId,
+            customer=self.stripe_customer_id,
             auto_advance=True,
             collection_method="charge_automatically",
-            default_payment_method=self.paymentMethod,
+            default_payment_method=self.payment_method,
             automatic_tax={"enabled": True},
         )
         stripe.InvoiceItem.create(
             unit_amount=settings.PRICE_PER_USER,
             quantity=num_users,
             currency="usd",
-            customer=self.stripeCustomerId,
+            customer=self.stripe_customer_id,
             description="Per member, per month",
             invoice=invoice,
             tax_behavior="exclusive",
