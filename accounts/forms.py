732cff4192d5e31b383a0ecc13c155c3fc1bcd18
Aaron Spindler <aaron@xnovax.net>
2023-02-02 21:40:34 -0500

    Profile Enhancements: chinese zodiac, zodiac sign, favourite food, favourite movie, and favourite travel destination (#123)
    * Added chinese zodiac, zodiac sign, favourite food, favourite movie, and favourite travel destination
    
    * Fixed URL for finding your zodiac sign
    
    * Rearranged form

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,42 @@ class UserProfileForm(forms.ModelForm):
     short_bio = forms.CharField(
         required=False, widget=forms.Textarea(attrs={"rows": "3"})
     )
+    favourite_food = forms.CharField(
+        required=False, widget=forms.Textarea(attrs={"rows": "2"})
+    )
+    favourite_movie = forms.CharField(
+        required=False, widget=forms.Textarea(attrs={"rows": "2"})
+    )
+    favourite_travel_destination = forms.CharField(
+        required=False, widget=forms.Textarea(attrs={"rows": "2"})
+    )
     profile_picture = forms.ImageField(label="", required=False)
+
+    class Meta:
+        model = User
+        fields = [
+            "profile_picture",
+            "short_bio",
+            "first_name",
+            "last_name",
+            "title",
+            "general_location",
+            "team",
+            "personality_type",
+            "linkedin",
+            "twitter",
+            "github",
+            "chinese_zodiac",
+            "zodiac_sign",
+            "favourite_food",
+            "favourite_movie",
+            "favourite_travel_destination",
+            "address_1",
+            "city",
+            "prov_state",
+            "postal_code",
+            "country",
+        ]
 
     def __init__(self, *args, **kwargs):
         company = kwargs.pop("company")
@@ -32,6 +67,36 @@ class UserProfileForm(forms.ModelForm):
         self.fields["personality_type"].widget.attrs.update(
             {
                 "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
+            }
+        )
+
+        self.fields["chinese_zodiac"].widget.attrs.update(
+            {
+                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
+            }
+        )
+
+        self.fields["zodiac_sign"].widget.attrs.update(
+            {
+                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
+            }
+        )
+
+        self.fields["favourite_food"].widget.attrs.update(
+            {
+                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            }
+        )
+
+        self.fields["favourite_movie"].widget.attrs.update(
+            {
+                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            }
+        )
+
+        self.fields["favourite_travel_destination"].widget.attrs.update(
+            {
+                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             }
         )
 
@@ -113,24 +178,3 @@ class UserProfileForm(forms.ModelForm):
                 "class": "ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             }
         )
-
-    class Meta:
-        model = User
-        fields = [
-            "profile_picture",
-            "short_bio",
-            "first_name",
-            "last_name",
-            "title",
-            "general_location",
-            "team",
-            "personality_type",
-            "linkedin",
-            "twitter",
-            "github",
-            "address_1",
-            "city",
-            "prov_state",
-            "postal_code",
-            "country",
-        ]
