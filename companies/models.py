431e11fbb5213ed5d94e0cdee234cc55607e66ac
Aaron Spindler <aaron@xnovax.net>
2023-02-22 20:36:30 -0500

    Fixed model names and admin filtering

--- companies/models.py
+++ companies/models.py
@@ -131,7 +131,7 @@ class Team(models.Model):
     name = models.CharField(max_length=60)
 
     def __str__(self):
-        return f"{self.company} {self.name}"
+        return f"{self.name}"
 
     class Meta:
         unique_together = ("company", "name")
@@ -146,7 +146,7 @@ class Location(models.Model):
     name = models.CharField(max_length=60)
 
     def __str__(self):
-        return f"{self.company} {self.name}"
+        return f"{self.name}"
 
     class Meta:
         unique_together = ("company", "name")
