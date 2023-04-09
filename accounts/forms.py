9092c5aaaa24204124f42d8e13edd3e77b6627b9
Aaron Spindler <aaron@xnovax.net>
2023-04-09 13:28:49 -0400

    Clean up profile and make it into sections

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from accounts.models import Pet, User
 from companies.models import Location, Team
 
+basic_field_class = "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+
 
 class PetForm(forms.ModelForm):
     picture = forms.ImageField(label="", required=False)
@@ -13,24 +15,12 @@ class PetForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(PetForm, self).__init__(*args, **kwargs)
-        self.fields["name"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
-        self.fields["nickname"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
-        self.fields["pet_type"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["name"].widget.attrs.update({"class": basic_field_class})
+        self.fields["nickname"].widget.attrs.update({"class": basic_field_class})
+        self.fields["pet_type"].widget.attrs.update({"class": basic_field_class})
         self.fields["picture"].widget.attrs.update(
             {
-                "class": "rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                "class": "rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             }
         )
 
@@ -79,69 +69,36 @@ class UserProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         company = kwargs.pop("company")
         super(UserProfileForm, self).__init__(*args, **kwargs)
+
         self.fields["general_location"].queryset = Location.objects.filter(
             company=company
         )
         self.fields["general_location"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
+            {"class": basic_field_class}
         )
 
         self.fields["team"].queryset = Team.objects.filter(company=company)
-        self.fields["team"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["team"].widget.attrs.update({"class": basic_field_class})
 
         self.fields["personality_type"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
+            {"class": basic_field_class}
         )
 
-        self.fields["chinese_zodiac"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["chinese_zodiac"].widget.attrs.update({"class": basic_field_class})
 
-        self.fields["zodiac_sign"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["zodiac_sign"].widget.attrs.update({"class": basic_field_class})
 
-        self.fields["favourite_food"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            }
-        )
+        self.fields["favourite_food"].widget.attrs.update({"class": basic_field_class})
 
-        self.fields["favourite_movie"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            }
-        )
+        self.fields["favourite_movie"].widget.attrs.update({"class": basic_field_class})
 
         self.fields["favourite_travel_destination"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            }
+            {"class": basic_field_class}
         )
 
-        self.fields["title"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["title"].widget.attrs.update({"class": basic_field_class})
 
-        self.fields["short_bio"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            }
-        )
+        self.fields["short_bio"].widget.attrs.update({"class": basic_field_class})
 
         self.fields["linkedin"].widget.attrs.update(
             {
@@ -161,51 +118,43 @@ class UserProfileForm(forms.ModelForm):
             }
         )
 
-        self.fields["first_name"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["first_name"].widget.attrs.update({"class": basic_field_class})
 
-        self.fields["last_name"].widget.attrs.update(
-            {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm"
-            }
-        )
+        self.fields["last_name"].widget.attrs.update({"class": basic_field_class})
 
         self.fields["address_1"].widget.attrs.update(
             {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm",
+                "class": basic_field_class,
                 "autocomplete": "street-address",
             }
         )
         self.fields["city"].widget.attrs.update(
             {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm",
+                "class": basic_field_class,
                 "autocomplete": "city",
             }
         )
         self.fields["prov_state"].widget.attrs.update(
             {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm",
+                "class": basic_field_class,
                 "autocomplete": "state",
             }
         )
         self.fields["postal_code"].widget.attrs.update(
             {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm",
+                "class": basic_field_class,
                 "autocomplete": "country",
             }
         )
         self.fields["country"].widget.attrs.update(
             {
-                "class": "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm",
+                "class": basic_field_class,
                 "autocomplete": "postal-code",
             }
         )
 
         self.fields["profile_picture"].widget.attrs.update(
             {
-                "class": "ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                "class": "rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             }
         )
