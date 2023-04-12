ec8c3c173f7c0cebcf4f36ce56eb5fc46b08d4ec
Aaron Spindler <aaron@xnovax.net>
2023-04-12 13:37:15 -0400

    Updated edit profile field width

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from accounts.models import Pet, User
 from companies.models import Location, Team
 
-BASIC_FIELD_CLASS = "block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+BASIC_FIELD_CLASS = "block w-full max-w-xxl rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 LINK_FIELD_CLASS = "block w-full min-w-0 flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 PICTURE_FIELD_CLASS = "rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 
@@ -29,7 +29,7 @@ class PetForm(forms.ModelForm):
 
 class UserProfileForm(forms.ModelForm):
     short_bio = forms.CharField(
-        required=False, widget=forms.Textarea(attrs={"rows": "3"})
+        required=False, widget=forms.Textarea(attrs={"rows": "5"})
     )
     favourite_food = forms.CharField(
         required=False, widget=forms.Textarea(attrs={"rows": "2"})
