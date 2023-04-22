5ab5aa2af31632d12d8652fb9e1a7b57c64beef0
Aaron Spindler <aaron@xnovax.net>
2023-04-22 11:43:45 -0400

    Working on script to make fake data

--- accounts/factories.py
+++ accounts/factories.py
@@ -1,7 +1,13 @@
+import random
+
 import factory
 
-from accounts.models import User
+from accounts.models import Pet, PetType, User
 from companies.factories import CompanyFactory
+
+PERSONALITY_TYPE_CHOICES = [x[0] for x in User.PERSONALITY_TYPE_CHOICES]
+CHINESE_ZODIAC_CHOICES = [x[0] for x in User.CHINESE_ZODIAC_CHOICES]
+ZODIAC_CHOICES = [x[0] for x in User.ZODIAC_SIGN_CHOICES]
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -14,7 +20,52 @@ class UserFactory(factory.django.DjangoModelFactory):
     company = factory.SubFactory(CompanyFactory)
     short_bio = factory.Faker("text", max_nb_chars=240)
     title = factory.Faker("job")
+    is_active = True
 
     @factory.lazy_attribute
     def username(self):
         return f"{self.first_name}{self.last_name}"
+
+    @factory.lazy_attribute
+    def team(self):
+        return random.choice(self.company.teams.all())
+
+    @factory.lazy_attribute
+    def general_location(self):
+        return random.choice(self.company.locations.all())
+
+    @factory.lazy_attribute
+    def personality_type(self):
+        return random.choice(PERSONALITY_TYPE_CHOICES)
+
+    @factory.lazy_attribute
+    def chinese_zodiac(self):
+        return random.choice(CHINESE_ZODIAC_CHOICES)
+
+    @factory.lazy_attribute
+    def zodiac_sign(self):
+        return random.choice(ZODIAC_CHOICES)
+
+    @factory.lazy_attribute
+    def linkedin(self):
+        return f"{self.first_name}{self.last_name}"
+
+    @factory.lazy_attribute
+    def twitter(self):
+        return f"{self.first_name}{self.last_name}"
+
+    @factory.lazy_attribute
+    def github(self):
+        return f"{self.first_name}{self.last_name}"
+
+
+class PetFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Pet
+
+    name = factory.Faker("first_name")
+    owner = factory.SubFactory(UserFactory)
+
+    @factory.lazy_attribute
+    def pet_type(self):
+        return random.choice(list(PetType.objects.all()))
