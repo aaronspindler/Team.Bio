105b8d284f00dd98838205b5393ce1b916e6e8c8
Aaron Spindler <aaron@xnovax.net>
2023-09-20 18:11:16 -0400

    Fixed some spacing issues

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from utils.forms import CleanedImageField
 
 BASIC_FIELD_CLASS = "block w-full max-w-xxl rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 LINK_FIELD_CLASS = "block w-full min-w-0 flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-PICTURE_FIELD_CLASS = "rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+PICTURE_FIELD_CLASS = "pl-3 rounded-md border border-gray-300 bg-white text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 
 
 class PetForm(forms.ModelForm):
