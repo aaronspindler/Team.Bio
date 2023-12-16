b039b16364e5976403b8817b1220dc5d7351ae54
Aaron Spindler <aaron@xnovax.net>
2023-12-16 14:58:01 -0500

    Fixed tests

--- companies/context_processors.py
+++ companies/context_processors.py
@@ -1,5 +1,6 @@
 def is_company_owner(request):
     is_owner = False
-    if request.user.pk in request.user.company.get_owners:
+    user = request.user
+    if user.is_authenticated and user.company and user.pk in user.company.get_owners:
         is_owner = True
     return {"is_company_owner": is_owner}
