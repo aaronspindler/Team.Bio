fb3147595d614243c78829e50aa9880eea1a31de
Aaron Spindler <aaron@xnovax.net>
2024-08-29 18:15:27 -0400

    Context processor

--- config/settings.py
+++ config/settings.py
@@ -109,6 +109,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "config.context_processors.export_required_vars",
                 "companies.context_processors.is_company_owner",
+                "django.core.context_processors.csrf",
             ],
         },
     },
